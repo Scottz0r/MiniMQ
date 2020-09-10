3b8a24c9e032f169476351f20fc1e3dcd0be2f69
Scott <Scottz0r@users.noreply.github.com>
2020-09-10 14:24:13 -0500

    Initial commit.

---
+++ MiniMQ/MQAsyncUserToken.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+
+namespace MiniMQ
+{
+    public enum MessageType
+    {
+        Unknown = -1,
+        Ack = 0,
+        PutMessage = 1,
+    }
+
+    public class CollectionState
+    {
+        public MessageType MessageType { get; set; } = MessageType.Unknown;
+
+        public int BodySize { get; set; } = 0;
+
+        public int CollectedBodyBytes { get; set; } = 0;
+    }
+
+    public class MQAsyncUserToken
+    {
+        public Guid Id { get; }
+
+        public Socket Socket { get; }
+
+        public DateTime LastActivity { get; set; }
+
+        public CollectionState CollectionState { get; set; }
+
+        // Buffer for network IO.
+        public byte[] Buffer { get; set; }
+
+        // TODO: Being lazy, for now, and allocating a new buffer of the exact size of the full message. This
+        // object will own this memory.
+        public byte [] MessageBody { get; set; }
+
+        public MQAsyncUserToken(Socket socket)
+        {
+            Id = Guid.NewGuid();
+            LastActivity = DateTime.Now;
+            CollectionState = new CollectionState();
+
+            Socket = socket;
+        }
+    }
+}
---
+++ MiniMQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using System;
+using System.Net;
+using System.Threading;
+
+namespace MiniMQ
+{
+    class ThreadIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddPropertyIfAbsent(propertyFactory.CreateProperty(
+                    "ThreadId", Thread.CurrentThread.ManagedThreadId));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.With(new ThreadIdEnricher())
+                .WriteTo.Console(
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] ({ThreadId}) {Message:lj}{NewLine}{Exception}"
+                )
+                .MinimumLevel.Debug()
+                .CreateLogger();
+
+            Log.Information("Mini MQ - a lightweight Message Queue.");
+
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            var s = new MQServer();
+            s.Init();
+            s.Start(localEndPoint);
+
+            Environment.Exit(0);
+        }
+    }
+}
